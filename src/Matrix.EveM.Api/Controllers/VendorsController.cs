78ab4ebd021f9ab8139df50841bf216e3bae5f95
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2023-06-17 19:22:17 +0530

    docs :memo: add controller documentation

--- src/Matrix.EveM.Api/Controllers/VendorsController.cs
+++ src/Matrix.EveM.Api/Controllers/VendorsController.cs
@@ -1,10 +1,14 @@
 using Matrix.EveM.Contracts.Vendors.Requests;
 using Matrix.EveM.Contracts.Vendors.Responses;
+using Matrix.EveM.Domain.Vendors.Entities;
 using Matrix.EveM.Domain.Vendors.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Matrix.EveM.Api.Controllers;
 
+/// <summary>
+/// The <see cref="VendorsController"/> class defines the API endpoints for <see cref="Vendor"/>.
+/// </summary>
 [ApiController]
 [Route("api/[controller]")]
 public class VendorsController : ControllerBase
@@ -16,15 +20,31 @@ public class VendorsController : ControllerBase
         _vendorService = vendorService;
     }
 
+    /// <summary>
+    /// The endpoint allows users to retrieve a vendor by it's ID.
+    /// </summary>
+    /// <param name="vendorId"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>A <see cref="Vendor"/>.</returns>
+    /// <reposnse code="200">If successful.</reposnse>
+    /// <response code="404">If the request is invalid.</response>
     [HttpGet]
     [Route("{vendorId:int}", Name = "GetVendorById}")]
     [ProducesResponseType(typeof(VendorDto), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<VendorDto> GetVendorByIdAsync(int vendorId, CancellationToken cancellationToken = default)
+    public async Task<VendorDto> GetVendorByIdAsync(
+        [FromRoute] int vendorId,
+        CancellationToken cancellationToken = default)
     {
         return await _vendorService.GetVendorByIdAsync(vendorId, cancellationToken);
     }
     
+    /// <summary>
+    /// The endpoint allows users to retrieve a list of vendors.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns>A list of vendors.</returns>
+    /// <reposnse code="200">If successful.</reposnse>
     [HttpGet]
     [ProducesResponseType(typeof(IEnumerable<VendorDto>), StatusCodes.Status200OK)]
     public async Task<IEnumerable<VendorDto>> GetVendorsAsync(CancellationToken cancellationToken = default)
@@ -32,34 +52,69 @@ public class VendorsController : ControllerBase
         return await _vendorService.GetVendorsAsync(cancellationToken);
     }
     
+    /// <summary>
+    /// The endpoint allows users to create a new vendor.
+    /// </summary>
+    /// <param name="createVendorRequest"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    /// <reposnse code="201">If successful</reposnse>
+    /// <response code="400">If the request is invalid</response>
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> CreateVendorAsync(
-        CreateVendorRequest createVendorRequest,
+        [FromBody] CreateVendorRequest createVendorRequest,
         CancellationToken cancellationToken = default)
     {
         var vendorId = await _vendorService.CreateVendorAsync(createVendorRequest, cancellationToken);
-        return Created(Url.RouteUrl("GetVendorById", new { vendorId }), vendorId);
+        return Created($"api/vendors/{vendorId}", vendorId);
     }
     
+    /// <summary>
+    /// The endpoint allows users to update a vendor by it's ID.
+    /// </summary>
+    /// <param name="vendorId"></param>
+    /// <param name="updateVendorRequest"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    /// <reposnse code="204">If successful</reposnse>
+    /// <response code="400">If the request is invalid</response>
+    /// <response code="404">If the vehicle feature is not found</response>
     [HttpPut]
+    [Route("{vendorId:int}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> UpdateVendorAsync(
-        UpdateVendorRequest updateVendorRequest,
+        [FromRoute] int vendorId,
+        [FromBody] UpdateVendorRequest updateVendorRequest,
         CancellationToken cancellationToken = default)
     {
+        if (vendorId != updateVendorRequest.Id)
+        {
+            return BadRequest();
+        }
+        
         await _vendorService.UpdateVendorAsync(updateVendorRequest, cancellationToken);
         return NoContent();
     }
     
+    /// <summary>
+    /// The endpoint allows users to delete a vendor by it's ID.
+    /// </summary>
+    /// <param name="vendorId"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    /// <reposnse code="204">If successful</reposnse>
+    /// <response code="404">If the vehicle feature is not found</response>
     [HttpDelete]
     [Route("{vendorId:int}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> DeleteVendorAsync(int vendorId, CancellationToken cancellationToken = default)
+    public async Task<IActionResult> DeleteVendorAsync(
+        [FromRoute] int vendorId,
+        CancellationToken cancellationToken = default)
     {
         await _vendorService.DeleteVendorAsync(vendorId, cancellationToken);
         return NoContent();
--- src/Matrix.EveM.Api/Program.cs
+++ src/Matrix.EveM.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Matrix.EveM.Dal;
 using Matrix.EveM.Service;
 
@@ -8,7 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddDataAccessLayer(builder.Configuration);
 builder.Services.AddApplicationServices();
